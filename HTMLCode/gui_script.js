5dcc2d08b7c2faf3d713eaac73b45c1d9a1602fe
Finix Lei <finixlei@gmail.com>
2025-03-09 19:39:55 +0800

    Restrict Moves

--- HTMLCode/gui_script.js
+++ HTMLCode/gui_script.js
@@ -1,4 +1,65 @@
-import {R1, R2, R3, B1, B2, B3, EMPTY} from "./constants.js"
+// import {R1, R2, R3, B1, B2, B3, EMPTY} from "./constants.js"
+
+/**
+ *  Constants Variables Part
+ */
+
+const PIECE_COLOR = {
+    RED: 'Red',
+    BLACK: 'Black'
+}
+
+const ROUTING = {
+    0: [1, 3, 4],
+    1: [0, 2, 4],
+    2: [1, 4, 5],
+    3: [0, 4, 6],
+    4: [0, 1, 2, 3, 5, 6, 7, 8],
+    5: [2, 4, 8],
+    6: [3, 4, 7],
+    7: [4, 6, 8],
+    8: [4, 5, 7]
+}
+
+const LINES = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+const R1 = 'R1';
+const R2 = 'R2';
+const R3 = 'R3';
+const B1 = 'B1';
+const B2 = 'B2';
+const B3 = 'B3';
+const EMPTY = 'E';
+
+const RED_STONES   = [R1, R2, R3]
+const BLACK_STONES = [B1, B2, B3]
+
+const RED_INIT_POSITIONS = {
+    R1: 0,
+    R2: 1,
+    R3: 2
+}
+
+const BLACK_INIT_POSITIONS = {
+    B1: 6,
+    B2: 7,
+    B3: 8
+}
+
+const INIT_BOARD = [R1, R2, R3, EMPTY, EMPTY, EMPTY, B1, B2, B3]
+
+const MAX_SCORE = 1000
+const MIN_SCORE = -1000
+
 
 // 定义 9 个有效落子位置的坐标
 const validPositions = [
@@ -21,7 +82,14 @@ const PIECE_RADIUS = 17.5
 const BOARD_WIDTH = 270
 const BOARD_HEIGHT = 270
 
+/**
+ * Global Variables
+ */
 let GUI_BOARD = [R1, R2, R3, EMPTY, EMPTY, EMPTY, B1, B2, B3]
+
+/**
+ * Utility Functions
+ */
 
 function validatePosition(x, y) {
     // for (const position of validPositions) {
@@ -40,6 +108,30 @@ function validatePosition(x, y) {
         }
     }
     return null;
+}
+
+// Make a move in GUI_BOARD
+function makeMove(pieceId, newPosition) {
+    // 找到当前棋子的位置
+    for (let i = 0; i < GUI_BOARD.length; i++) {
+        if (GUI_BOARD[i] == pieceId) {
+            GUI_BOARD[i] = EMPTY;
+            break;
+        }
+    }
+
+    let i = 0;
+    for (; i < validPositions.length; i++) {
+        if (validPositions[i].x == newPosition.x && validPositions[i].y == newPosition.y) {
+            break;
+        }
+        i ++;
+    }
+    if (i == validPositions.length) {
+        return;
+    }
+    GUI_BOARD[i] = pieceId;
+    console.log(GUI_BOARD)
 }
 
 
@@ -93,15 +185,16 @@ document.addEventListener('DOMContentLoaded', () => {
         else {
             x = new_position.x
             y = new_position.y
-        }
+            // alert(selectedPiece.id);
+            makeMove(selectedPiece.id, new_position)
 
-        // alert("x=" + x + ", y=" + y)
-        // 设置选中棋子的新位置
-        selectedPiece.style.left = `${x}px`;
-        selectedPiece.style.top = `${y}px`;
-        // 恢复选中棋子的大小
-        selectedPiece.style.transform = 'scale(1)';
-        // 取消选中状态
-        selectedPiece = null;
+            // 设置选中棋子的新位置
+            selectedPiece.style.left = `${x}px`;
+            selectedPiece.style.top = `${y}px`;
+            // 恢复选中棋子的大小
+            selectedPiece.style.transform = 'scale(1)';
+            // 取消选中状态
+            selectedPiece = null;
+        }
     });
 });
